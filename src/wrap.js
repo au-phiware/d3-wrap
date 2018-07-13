a9da9f7412e80b6bba82c9e3d1b4e468e854dd98
Corin Lawson <corin@phiware.com.au>
2018-07-14 02:39:25 +1000

    Ensure the function under wrap is also extended
    Prior to this change only the returned, wrapped function was extended
    with the properties of the wrapped function. The function that is passed
    to the wrapping function (this is the function that wraps the selection
    and ensure any transition is carried forward) did not have any of the
    properties of the wrapped function.
    
    This change extends the function that is passed to the wrapping function
    with the properties of the wrapped function.

--- src/wrap.js
+++ src/wrap.js
@@ -1,11 +1,20 @@
-var nonEnumerableProps = /^(valueOf|isPrototypeOf|to(Locale)?String|propertyIsEnumerable|hasOwnProperty)$/;
+const nonEnumerableProps = /^(valueOf|isPrototypeOf|to(Locale)?String|propertyIsEnumerable|hasOwnProperty)$/;
+
+function extend(f, wrapped) {
+  for (var k in f) {
+    if (!nonEnumerableProps.test(k)) {
+      wrapped[k] = f[k];
+    }
+  }
+  return wrapped;
+}
 
 export default function wrap(f, wrapper) {
-  var wrapped = function(context) {
+  return extend(f, function(context) {
     for (var i = 0, ii = arguments.length, args = Array(ii + 1); i < ii; i++) {
       args[i + 1] = arguments[i];
     }
-    args[0] = function(wrappedContext) {
+    args[0] = extend(f, function(wrappedContext) {
       for (var i = 0, ii = arguments.length, args = Array(ii); i < ii; i++) {
         args[i] = arguments[i];
       }
@@ -13,13 +22,7 @@ export default function wrap(f, wrapper) {
         args[0] = wrappedContext.transition(context);
       }
       return f.apply(this, args);
-    };
+    });
     return wrapper.apply(this, args);
-  }
-  for (var k in f) {
-    if (!nonEnumerableProps.test(k)) {
-      wrapped[k] = f[k];
-    }
-  }
-  return wrapped;
+  });
 }
